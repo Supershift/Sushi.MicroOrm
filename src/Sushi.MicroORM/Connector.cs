a4a5abc292182f798c8782d5676062b01ca938f4
Mark de Vries <mark.devries@supershift.nl>
2021-12-15 09:13:50 +0100

    upgraded to net6, replaced system.data.sqlclient with microsoft.data.sqlclient

--- src/Sushi.MicroORM/Connector.cs
+++ src/Sushi.MicroORM/Connector.cs
@@ -1,10 +1,10 @@
-﻿using Sushi.MicroORM.Mapping;
+﻿using Microsoft.Data.SqlClient;
+using Sushi.MicroORM.Mapping;
 using Sushi.MicroORM.Supporting;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Reflection;
 using System.Threading;

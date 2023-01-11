839d8895e324b82f1b21dd177d69f0adeb6f0688
Boris Janjic <boris.janjic@supershift.nl>
2023-01-11 15:19:40 +0100

    Added inlinedata for GetSqlDbTypeTest in UtilityTest

--- tests/Sushi.MicroORM.UnitTests/UtilityTest.cs
+++ tests/Sushi.MicroORM.UnitTests/UtilityTest.cs
@@ -11,6 +11,20 @@ namespace Sushi.MicroORM.UnitTests
     {
         [Theory]
         [InlineData(typeof(int), SqlDbType.Int)]
+        [InlineData(typeof(long), SqlDbType.BigInt)]
+        [InlineData(typeof(short), SqlDbType.SmallInt)]
+        [InlineData(typeof(byte), SqlDbType.TinyInt)]
+        [InlineData(typeof(bool), SqlDbType.Bit)]
+        [InlineData(typeof(Guid), SqlDbType.UniqueIdentifier)]
+        [InlineData(typeof(DateTime), SqlDbType.DateTime)]
+        [InlineData(typeof(DateTimeOffset), SqlDbType.DateTimeOffset)]
+        [InlineData(typeof(DateOnly), SqlDbType.Date)]
+        [InlineData(typeof(decimal), SqlDbType.Decimal)]
+        [InlineData(typeof(double), SqlDbType.Decimal)]
+        [InlineData(typeof(float), SqlDbType.Decimal)]
+        [InlineData(typeof(TimeSpan), SqlDbType.Time)]
+        [InlineData(typeof(TimeOnly), SqlDbType.Time)]
+        [InlineData(typeof(Byte[]), SqlDbType.VarBinary)]
         public void GetSqlDbTypeTest(Type type, SqlDbType expected)
         {
             var result = Utility.GetSqlDbType(type);
